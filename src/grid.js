30e9ebfcd0750ef8df03b81b625b516481309b4f
RAFAEL SCHUSTER <rsc1335@gmail.com>
2020-11-23 23:28:18 +0200

    WIP Milestone1

--- src/grid.js
+++ src/grid.js
@@ -1,16 +1,15 @@
 import React from 'react'
 let counter = 0;
-let arrNotes = []
+let arrNotes = [];
 
 class Grid extends React.Component{
     render(){
-        const myNotes = Object.values(this.props.notes)
-        console.log(myNotes)
-        arrNotes[counter] = myNotes
-        counter++
+        const myNotes = Object.values(this.props.notes);
+        arrNotes[counter] = myNotes;
+        counter++;
     return(
         <div> 
-              {arrNotes.map(note => <p key = {counter}>{note[0]} {note[1]}</p>)} 
+              {arrNotes.map(note => <p key = {counter}>{note[0]} {note[1]}</p>)} ;
         </div>
 
     )
